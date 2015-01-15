683652a6f2daff02050ccac82d421ad47e84ffd9
spinlocos <glissando7@gmail.com>
2015-01-16 01:03:15 +0200

    ti kodingk stail ine afto mori

--- Parser.java
+++ Parser.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Parser{
+public class Parser
+{
     static int[] inputInts;
     static char[][] map;
 
-    public Parser(String fil) {
-        try
-        {
+    public Parser(String fil)
+    {
+        try {
             char ch;
             BufferedReader in =
                 new BufferedReader(new FileReader(fil));
@@ -15,41 +16,43 @@ public class Parser{
             Scanner a = new Scanner(in);
             inputInts = new int[8];
 
-            for(i = 0; i<8 ; i++)
+            for (i = 0; i < 8; i++)
                 inputInts[i] = a.nextInt();
 
-            map = new char [inputInts[1]] [inputInts[0]];
+            map = new char [inputInts[1]][inputInts[0]];
 
             String str;
 
-            for(i=0; i< inputInts[1]; i++){
+            for (i = 0; i < inputInts[1]; i++){
                 str = a.next();
-                map[i]=str.toCharArray();
+                map[i] = str.toCharArray();
             }
             in.close();
         }
-        catch(Exception e){
+        catch (Exception e){
             e.printStackTrace();
         }
     }
 
-    public int[] getInputInts(){
+    public int[] getInputInts()
+    {
         return inputInts;
     }
 
-    public char[][] getMap(){
+    public char[][] getMap()
+    {
         return map;
     }
 
     public Vertex[][] getVertexMap()
     {
         Vertex[][] vMap = new Vertex[inputInts[1]][inputInts[0]];
-        for(int i=0; i<inputInts[1]; i++)
-            for(int j=0; j<inputInts[0]; j++)
-                vMap[i][j] = new Vertex(i,j,map[i][j]);
+        for (int i = 0; i < inputInts[1]; i++)
+            for (int j = 0; j < inputInts[0]; j++)
+                vMap[i][j] = new Vertex(i, j, map[i][j]);
 
-        for(int i=0; i<inputInts[1]; i++)
-            for(int j=0; j<inputInts[0]; j++)
+        for (int i = 0; i < inputInts[1]; i++)
+            for (int j = 0; j < inputInts[0]; j++)
                 vMap[i][j].findNeighbours(inputInts[1], inputInts[0], vMap);
         return vMap;
     }
