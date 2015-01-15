c7d00babf67b8f28f081fe0585e33a9959aac7d1
Helene <helm763@gmail.com>
2015-01-15 22:31:59 +0200

    texniti???

---
+++ StarSolver.java
@@ -0,0 +1,72 @@
+import java.util.PriorityQueue;
+import java.util.Comparator;
+import java.util.ArrayList;
+
+public class StarSolver
+{
+    public int xMax, yMax, initX, initY, goalX, goalY;
+    Vertex[][] map;
+
+    public StarSolver(int xM,int yM, int inX, int inY,
+            int gX, int gY, Vertex[][] grid)
+    {
+        xMax = xM;
+        yMax = yM;
+        initX = inX;
+        initY = inY;
+        goalX = gX;
+        goalY = gY;
+        map = grid;
+    }
+
+    private Comparator<Vertex> gCompare = new Comparator<Vertex>()
+    {
+        @Override
+        public int compare(Vertex v1, Vertex v2) {
+            return (v1.gScore+v1.uCost(goalX, goalY) -
+                    (v2.gScore+v2.uCost(goalX, goalY)));
+        }
+    };
+
+    public ArrayList<Vertex> solver()
+    {
+        Vertex start = map[initX][initY];
+        Vertex goal = map[goalX][goalY];
+
+        PriorityQueue<Vertex> openQueue = new PriorityQueue<Vertex>(100, gCompare);
+        ArrayList<Vertex> exploredSet = new ArrayList<Vertex>();
+
+        openQueue.add(start);
+        int num = 0;
+
+        while (!openQueue.isEmpty()) {
+
+            Vertex curr = openQueue.remove();
+            if (curr.equals(goal)){
+                System.out.println("NUMBER OF ITERS " +num);
+                return curr.path();
+            }
+            exploredSet.add(curr);
+
+            for (Vertex tmp : curr.neighbourList) {
+                num++;
+                int gSc = curr.gScore + 1;
+
+                if ((exploredSet.contains(tmp)) && (gSc < tmp.gScore)) {
+                    exploredSet.remove(tmp);
+                }
+
+                if ((openQueue.contains(tmp)) && (gSc < tmp.gScore)) {
+                    openQueue.remove(tmp);
+                }
+
+                if ((!openQueue.contains(tmp)) && (!exploredSet.contains(tmp))) {
+                    tmp.setParent(curr);
+                    tmp.gScore = gSc;
+                    openQueue.add(tmp);
+                }
+            }
+        }
+        return null;
+    }
+}
---
+++ Vertex.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class Vertex
+{
+    public int x, y, gScore;
+    public Vertex parent;
+    public ArrayList <Vertex> neighbourList;
+    private boolean flag = false;
+    char type ;
+
+    public Vertex(int xCord, int yCord , char type)
+    {
+        x = xCord;
+        y = yCord;
+        gScore = 0;
+        parent = null;
+        this.type = type;
+    }
+
+    public void setParent(Vertex par)
+    {
+        parent = par;
+    }
+
+    public void printme()
+    {
+        System.out.println("X:" + x + " Y:" + y);
+    }
+
+    public boolean equals(Vertex vert)
+    {
+        return (x==vert.x)&&(y==vert.y);
+    }
+
+    public void findNeighbours(int maxX, int maxY, Vertex[][] vMap)
+    {
+        neighbourList = new ArrayList<Vertex>();
+
+        if ((x-1 >= 0) && (vMap[x-1][y].type == 'O'))
+            neighbourList.add(vMap[x-1][y]);
+        if ((x+1 < maxX) && (vMap[x+1][y].type == 'O'))
+            neighbourList.add(vMap[x+1][y]);
+        if ((y-1 >= 0) && (vMap[x][y-1].type == 'O'))
+            neighbourList.add(vMap[x][y-1]);
+        if ((y+1 < maxY) && (vMap[x][y+1].type == 'O'))
+            neighbourList.add(vMap[x][y+1]);
+
+    }
+
+    public ArrayList<Vertex> path()
+    {
+        ArrayList<Vertex> vertList= new ArrayList<Vertex>();
+
+        Vertex curr = this;
+        while (curr != null){
+            vertList.add(curr);
+            curr = curr.parent;
+        }
+
+        return vertList;
+    }
+
+    public int uCost(int xGoal, int yGoal)
+    {
+        return Math.abs(x - xGoal) + Math.abs(y - yGoal);
+    }
+
+    public int oCost(int xGoal, int yGoal)
+    {
+        return 2 * Math.max(Math.abs(x - xGoal), Math.abs(y - yGoal));
+    }
+
+    public boolean isNeighbor(Vertex other)
+    {
+        if ((x-1 >= 0) && (other.x + 1 == x) && (other.y == y))
+            return true;
+        else if ((x+1 < StarSolver.xMax) && (other.x - 1 == x) && (other.y == y))
+            return true;
+        else if ((y-1 >= 0) && (other.y + 1 == y) && (other.x == x))
+            return true;
+        else if ((y+1 < StarSolver.yMax) && (other.y -1 == y) && (other.x == x))
+            return true;
+        else
+            return false;
+    }
+
+}
